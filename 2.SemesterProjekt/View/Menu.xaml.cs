1ba31b69b6141fdf2cd0990d041740f1caffe507
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-04-24 12:02:11 +0200

    *MainPage +Frame, frame er blevet tilføjet så vi kan navigere +Button, denne knap er vores hjem knap.
    *Menu
    +3 Knapper, til Wiki, Familie og Vaccination's Kort.
    Det hele ligge i et stackPanel. (deres view er ikke lagt endnu endnu.)

--- 2.SemesterProjekt/View/Menu.xaml.cs
+++ 2.SemesterProjekt/View/Menu.xaml.cs
@@ -13,19 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace _2.SemesterProjekt
+namespace _2.SemesterProjekt.View
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Menu : Page
     {
-        public MainPage()
+        public Menu()
         {
             this.InitializeComponent();
-            MainPageFrame.Navigate(typeof(View.Menu));
         }
     }
 }
