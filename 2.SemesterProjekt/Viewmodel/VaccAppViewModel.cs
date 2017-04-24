a65e88768b49075b5942b2e87513ab4674a63710
Benjamin-pc <benjaminlindgaard@ka-net.dk>
2017-04-24 11:25:39 +0200

    Oprettede viewmodel

---
+++ 2.SemesterProjekt/Viewmodel/VaccAppViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2.SemesterProjekt.Model;
+namespace _2.SemesterProjekt.Viewmodel
+{
+   public class VaccAppViewModel
+    {
+        public Singleton Singletion { get; set; }
+
+        private int fødselsdato;
+
+        public int Fødselsdato
+        {
+            get { return fødselsdato;; }
+            set { fødselsdato = value; }
+        }
+
+        private string navn;
+
+        public string Navn
+        {
+            get { return navn; }
+            set { navn = value; }
+        }
+
+
+    }
+}
