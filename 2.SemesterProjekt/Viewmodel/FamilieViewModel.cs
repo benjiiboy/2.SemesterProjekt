4e01716de2233cc5a1c347d7d59b6c9112f9ee01
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-05-02 15:09:55 +0200

    *FamilieViewModel +Ctor, den er stadig tom men placeret +PropertyChangedEventHandler +Select handler + nedavering fra INotifyPropertyChanged

--- 2.SemesterProjekt/Viewmodel/FamilieViewModel.cs
+++ 2.SemesterProjekt/Viewmodel/FamilieViewModel.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using _2.SemesterProjekt.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace _2.SemesterProjekt.Viewmodel
 {
-    class FamilieViewModel
+    class FamilieViewModel : INotifyPropertyChanged
     {
+
+        //prop
+
+        //ctor
+        public FamilieViewModel()
+        {
+               
+        }
+
+        //standard se evt Gist.
+
+        #region vores PropertyChangedEventHandler 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        #region Select event prop & instance field
+
+        private static Barn selectedBarn;
+        public Barn SelectedBarn
+        {
+            get { return selectedBarn; }
+            set { selectedBarn = value; OnPropertyChanged(nameof(SelectedBarn)); }
+        }
+
+        #endregion
     }
 }
