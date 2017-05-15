ef57a531068bf8c002c1aab5ed61c8db72c92806
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-05-15 18:46:40 +0200

    *VacPlan, tilføjet så den stemmer overens med vores tabel i databasen, samt overskrevet tostring
    *VacPlan, slettet en del og ændret det til et view med overskrift samt ListView

---
+++ 2.SemesterProjekt/Model/VacPlan.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.SemesterProjekt.Model
+{
+    class VacPlan
+    {
+        public int Plan_Id;
+        public DateTime Tid;
+        public bool TrueFalse;
+        public string Note;
+        public string VaccineNavn;
+        public int Barn_Id;
+
+
+        public VacPlan(int planID, DateTime Tid, bool TrueFalse, string Note, string VaccineNavn, int Barn_Id)
+        {
+
+            this.Plan_Id = planID;
+            this.Tid = Tid;
+            this.TrueFalse = TrueFalse;
+            this.Note = Note;
+            this.VaccineNavn = VaccineNavn;
+            this.Barn_Id = Barn_Id;
+
+        }
+
+
+
+        public override string ToString()
+        {
+            return $"PlanID:{Plan_Id} {Tid} modtaget vac:{TrueFalse} note:{Note} navn på vac:{VaccineNavn}";
+        }
+
+    }
+}
+
+
