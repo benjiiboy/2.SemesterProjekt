68a2d8a5ea668f455bfe891ba282a4fba637ce95
Mik Kröis Pedersen <mikx0067@edu.easj.dk>
2017-05-15 18:58:20 +0200

    +*VacPlanViewmodel, tilføjet ny viewmodel +Inotifty +Selected +Ctor

---
+++ 2.SemesterProjekt/Viewmodel/VacPlanViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2.SemesterProjekt.Model;
+
+namespace _2.SemesterProjekt.Viewmodel
+{
+    class VacPlanViewModel : INotifyPropertyChanged
+    {
+
+        #region valg af plan
+        private VacPlan _valgtPlan;
+
+        public VacPlan ValgtPlan
+        {
+            get { return _valgtPlan; }
+            set
+            {
+                _valgtPlan = value;
+                OnPropertyChanged(nameof(ValgtPlan));
+            }
+        }
+#endregion
+
+
+
+        //ctor
+        public VacPlanViewModel()
+        {
+
+        }
+
+        #region propchanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
+
+
+    }
+}
