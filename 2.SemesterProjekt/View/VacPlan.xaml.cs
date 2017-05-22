f54a25bc8487529bed7d30ed66a4b3c5dabb2dd2
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-05-22 11:57:12 +0200

    +Instant toast "Husk Vaccine"

--- 2.SemesterProjekt/View/VacPlan.xaml.cs
+++ 2.SemesterProjekt/View/VacPlan.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +27,18 @@ namespace _2.SemesterProjekt.View
         public VacPlan()
         {
             this.InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            ToastTemplateType toastTemplate = ToastTemplateType.ToastImageAndText01;
+            XmlDocument toastxml = ToastNotificationManager.GetTemplateContent(toastTemplate);
+
+            IXmlNode toasttextelements = toastxml.GetElementsByTagName("text").FirstOrDefault();
+            toasttextelements.AppendChild(toastxml.CreateTextNode("Husk vaccine!"));
+
+            ToastNotification toast1 = new ToastNotification(toastxml);
+            ToastNotificationManager.CreateToastNotifier().Show(toast1);
         }
     }
 }
